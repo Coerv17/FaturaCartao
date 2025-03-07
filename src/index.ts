051d6a3b67271864523c07dcc6dee1189df6d9d7
Coerv17 <vryan073@gmail.com>
2025-03-07 20:28:39 -0300

    Sua mensagem de commit

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,9 @@ const app = express();
 
 app.use(express.json());
 
-// Importação e uso das rotas
-import createUser from "../src/router/user.router";
-import createCartoes from "../src/router/cartoes.router";
-import createDespesa from "../src/router/despesa.router";
-
-app.use("/user", createUser);
-app.use("/cartoes", createCartoes);
-app.use("/despesa", createDespesa);
-
-// Rota padrão para verificar se a API está no ar
+// Suas rotas aqui
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "API está no ar!" });
 });
 
-// Exporta o app como uma função serverless
 export default serverless(app);
